70cf38d6f4120dee192c2860d4bbb6530e1fc704
Mahendra <mahendra.aderu@6dtech.co.in>
2020-09-20 18:27:25 +0530

    Updates

---
+++ precache-manifest.4667a26cc10ddc02ee68b3ecc2ad324a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54010fbd4d956faab96bdb4c3c92a19e",
+    "url": "/todo_react/index.html"
+  },
+  {
+    "revision": "f9391b52d2afeb0d827c",
+    "url": "/todo_react/static/css/main.69dd7bcc.chunk.css"
+  },
+  {
+    "revision": "38db11453d246ad5a494",
+    "url": "/todo_react/static/js/2.0bcb3f6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo_react/static/js/2.0bcb3f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9391b52d2afeb0d827c",
+    "url": "/todo_react/static/js/main.01c05a03.chunk.js"
+  },
+  {
+    "revision": "4a974673aa53f5d8d225",
+    "url": "/todo_react/static/js/runtime-main.1c25592b.js"
+  }
+]);
